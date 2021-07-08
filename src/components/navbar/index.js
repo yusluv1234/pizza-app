a4851649016ef3c1b46e3ab701de0325e800df59
yusluv1234 <yusluv124@gmail.com>
2021-07-08 10:43:14 +0100

    first commit

---
+++ src/components/navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Nav, NavLink } from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Nav>
+      <NavLink to="/">Okaay ooo</NavLink>
+      </Nav>
+    </div>
+  )
+}
+
+export default Navbar
